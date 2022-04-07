32eb5ffb271fe1d223f21fc8ddecd38fc9a15d3a
XXX <XXX@mimiquate.com>
2022-04-07 11:45:59 -0300

    Process,,supervisor, and cache

--- apps/info_sys/lib/info_sys/application.ex
+++ apps/info_sys/lib/info_sys/application.ex
@@ -1,19 +1,23 @@
+# ---
+# Excerpted from "Programming Phoenix 1.4",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/phoenix14 for more book information.
+# ---
 defmodule InfoSys.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
-  @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: InfoSys.Worker.start_link(arg)
-      # {InfoSys.Worker, arg}
+      InfoSys.Cache,
+      {Task.Supervisor, name: InfoSys.TaskSupervisor},
+
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: InfoSys.Supervisor]
     Supervisor.start_link(children, opts)
   end
