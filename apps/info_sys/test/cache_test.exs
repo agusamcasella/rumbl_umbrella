4703b86601c9db0ad924dc6b54fca888006e090f
XXX <XXX@mimiquate.com>
2022-04-07 16:17:25 -0300

    testing the app/infosys/cache.ex

--- apps/info_sys/test/cache_test.exs
+++ apps/info_sys/test/cache_test.exs
@@ -3,12 +3,12 @@ defmodule InfoSysTest.CacheTest do
   alias InfoSys.Cache
   @moduletag clear_interval: 100
 
+  ############# helpers###########
   defp assert_shutdown(pid) do
     ref = Process.monitor(pid)
     Process.unlink(pid)
     Process.exit(pid, :kill)
-
-    assert_recieve({:DOWN, ^ref, :process, ^pid, :killed})
+    assert_receive {:DOWN, ^ref, :process, ^pid, :killed}
   end
 
   defp eventually(func) do
@@ -19,6 +19,8 @@ defmodule InfoSysTest.CacheTest do
       eventually(func)
     end
   end
+
+  ################################
 
   setup %{test: name, clear_interval: clear_interval} do
     {:ok, pid} = Cache.start_link(name: name, clear_interval: clear_interval)
@@ -35,5 +37,19 @@ defmodule InfoSysTest.CacheTest do
 
   test "unfound entry returns error", %{name: name} do
     assert Cache.fetch(name, :notexists) == :error
+  end
+
+  test "clears all entries after clear interval", %{name: name} do
+    assert :ok = Cache.put(name, :key1, :value1)
+    assert Cache.fetch(name, :key1) == {:ok, :value1}
+    assert eventually(fn -> Cache.fetch(name, :key1) == :error end)
+  end
+
+  @tag clear_interval: 60_000
+  test "values are cleaned up on exit", %{name: name, pid: pid} do
+    assert :ok = Cache.put(name, :key1, :value1)
+    assert_shutdown(pid)
+    {:ok, _cache} = Cache.start_link(name: name)
+    assert Cache.fetch(name, :key1) == :error
   end
 end
